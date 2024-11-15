43168f78078cfa2d98bcea34fc2e4e409d1892b7
searover <ibaiguang@gmail.com>
2024-11-15 16:01:23 +0800

    Add Avl Tree code.

---
+++ src/main/java/org/book/exercise/chapter04/AvlTree.java
@@ -0,0 +1,128 @@
+package org.book.exercise.chapter04;
+
+/**
+ * @author luxz
+ * @date 2024/11/14-22:10
+ */
+public class AvlTree<T extends Comparable<? super T>> {
+
+    private static class AvlNode<T> {
+        T element;
+        AvlNode<T> left;
+        AvlNode<T> right;
+        int height;
+
+        public AvlNode(T element) {
+            this(element, null, null);
+        }
+
+        public AvlNode(T element, AvlNode<T> left, AvlNode<T> right) {
+            this.element = element;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    /**
+     * Return the height of node t, or -1 if null.
+     *
+     * @param t
+     * @return
+     */
+    private int height(AvlNode<T> t) {
+        return t == null ? -1 : t.height;
+    }
+
+    /**
+     * Internal method to insert a subtree.
+     *
+     * @param x the item to insert.
+     * @param t the node that roots the subtree.
+     * @return the new root of the subtree.
+     */
+    private AvlNode<T> insert(T x, AvlNode<T> t) {
+        if (t == null)
+            return new AvlNode<>(x);
+
+        int compareResult = x.compareTo(t.element);
+        if (compareResult < 0)
+            t.left = insert(x, t.left);
+        else if (compareResult > 0)
+            t.right = insert(x, t.right);
+        else
+            ;   // Duplicate; do nothing
+//        return t;
+        return balance(t);
+    }
+
+    private static final int ALLOWED_IMBALANCE = 1;
+
+    private AvlNode<T> balance(AvlNode<T> t) {
+        if (t == null)
+            return t;
+
+        if (height(t.left) - height(t.right) > ALLOWED_IMBALANCE) {
+            if (height(t.left.left) >= height(t.left.right))
+                t = rotateWithLeftChild(t);
+            else
+                t = doubleWithLeftChild(t);
+        } else if (height(t.right) - height(t.left) > ALLOWED_IMBALANCE) {
+            if (height(t.right.right) >= height(t.right.left))
+                t = rotateWithRightChild(t);
+            else
+                t = doubleWithRightChild(t);
+        }
+        t.height = Math.max(height(t.left), height(t.right)) + 1;
+        return t;
+    }
+
+    /**
+     * Rotate binary tree node with left child.
+     * For AVL trees, this is a single rotation for case 1.
+     * Update heights, then return new root.
+     *
+     * @param k2
+     * @return
+     */
+    private AvlNode<T> rotateWithLeftChild(AvlNode<T> k2) {
+        AvlNode<T> k1 = k2.left;
+        k2.left = k1.right;
+        k1.right = k2;
+        k2.height = Math.max(height(k2.left), height(k2.right)) + 1;
+        k1.height = Math.max(height(k1.left), height(k1.right)) + 1;
+        return k1;
+    }
+
+    /**
+     * Double rotate binary tree node: first left child with its right child;
+     * then node k3 with new left child.
+     * For AVL trees, this is a double rotation for case 2.
+     * Update heights, then return new root.
+     *
+     * @param k3
+     * @return
+     */
+    private AvlNode<T> doubleWithLeftChild(AvlNode<T> k3) {
+        k3.left = rotateWithRightChild(k3.left);
+        return rotateWithLeftChild(k3);
+    }
+
+    private AvlNode<T> remove(T x, AvlNode<T> t) {
+        if (t == null)
+            return t;   // Item not found, do nothing
+
+        int compareResult = x.compareTo(t.element);
+        if (compareResult < 0) {
+            t.left = remove(x, t.left);
+        } else if (compareResult > 0) {
+            t.right = remove(x, t.right);
+        } else if (t.left != null && t.right != null) {// Two children
+            t.element = findMin(t.right).element;
+            t.right = remove(t.element, t.right);
+        } else {
+            t = (t.left != null) ? t.left : t.right;
+        }
+        return balance(t);
+    }
+
+}
